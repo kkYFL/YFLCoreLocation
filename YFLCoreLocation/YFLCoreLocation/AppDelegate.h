ef6e8561e97343d2e04a8f60dc44474b05e1d330
杨丰林 <xiaoyang@maihaoche.com>
2017-02-16 00:03:38 +0800

    初始化项目

---
+++ YFLCoreLocation/YFLCoreLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFLCoreLocation
+//
+//  Created by 杨丰林 on 17/2/16.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFLCoreLocation/YFLCoreLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFLCoreLocation
+//
+//  Created by 杨丰林 on 17/2/16.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
